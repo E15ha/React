27224dd3cdfe6af37db590bc1bca569fb0af3b43
E15ha <eishaiqbaliqbal@gmail.com>
2025-11-19 20:19:04 +0500

    Initial React project

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About Page</h1>
+      <p>Jenkins deployment test successful!</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Home Page</h1>
+      <p>This is your deployed React homepage.</p>
+    </div>
+  );
+}
+
+export default Home;
+
